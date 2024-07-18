865affe5f6995ce98cd0dbf375137eb8a95d8451
Thiebault Nicolas <nicolasthiebault80870@yahoo.com>
2024-07-18 16:24:31 +0200

    exercice 1 et 2 ok

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,28 @@
-//votre code ici
+function pairNumbers(min, max) {
+    // Initialisation de la chaîne de caractères qui contiendra les nombres pairs
+    let result = '';
+    
+    // Boucle pour parcourir les nombres de min à max
+    for (let i = min; i <= max; i++) {
+        // Vérification si le nombre est pair
+        if (i % 2 === 0) {
+            // Ajout du nombre pair et d'une virgule à la chaîne de caractères
+            result += i + ',';
+        }
+    }
+    
+    // Retirer la dernière virgule, s'il y a des nombres dans la chaîne
+    if (result.length > 0) {
+        result = result.slice(0, -1);
+    }
+    
+    // Retourne la chaîne de caractères finale
+    return result;
+}
 
-export default pairNumbers
+export default pairNumbers;
+
+// Exemple de tests pour vérifier la fonction pairNumbers
+console.log(pairNumbers(1, 10)); // Devrait afficher "2,4,6,8,10"
+console.log(pairNumbers(3, 7));  // Devrait afficher "4,6"
+console.log(pairNumbers(0, 5));  // Devrait afficher "0,2,4"
--- Exercice2/index.js
+++ Exercice2/index.js
@@ -1,11 +1,35 @@
 // Créez votre fonction ici
 
+function calculate(a, b, operator) {
+    // Utilisation de la structure switch pour déterminer l'opération à effectuer
+    switch (operator) {
+        case '+':
+            // Si l'opérateur est '+', retourner la somme de a et b
+            return a + b;
+        case '-':
+            // Si l'opérateur est '-', retourner la différence entre a et b
+            return a - b;
+        case '*':
+            // Si l'opérateur est '*', retourner le produit de a et b
+            return a * b;
+        case '/':
+            // Si l'opérateur est '/', vérifier si b est zéro pour éviter la division par zéro
+            if (b === 0) {
+                return "La division par zéro n'est pas autorisée"; // Retourner un message d'erreur si b est zéro
+            }
+            return a / b; // Sinon, retourner le quotient de a et b
+        default:
+            // Si l'opérateur n'est pas reconnu, retourner un message d'erreur
+            return "Opérateur invalide";
+    }
+}
+
 // Exemples d'utilisation de la fonction
 console.log(calculate(5, 3, '+'));   // Affiche 8
 console.log(calculate(10, 4, '-'));  // Affiche 6
 console.log(calculate(7, 2, '*'));   // Affiche 14
 console.log(calculate(12, 3, '/'));  // Affiche 4
-console.log(calculate(8, 0, '/'));   // Affiche "Division by zero is not allowed"
-console.log(calculate(4, 5, '%'));   // Affiche "Invalid operator"
+console.log(calculate(8, 0, '/'));   // Affiche "La division par zéro n'est pas autorisée"
+console.log(calculate(4, 5, '%'));   // Affiche "Opérateur invalide"
 
-export default calculate
+export default calculate;
